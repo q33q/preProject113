8f1898dfed1640a1575278f3323b698285ca4d8e
Pavel Kozhaev <zxc321z9@gmail.com>
2021-10-10 04:18:01 +0300

    Удалены комментарии из Main и метод Close из класса Util

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -3,9 +3,6 @@ package jm.task.core.jdbc.util;
 import java.sql.*;
 
 public class Util {
-    // реализуйте настройку соеденения с БД
-    private Connection connection = null;
-    private Statement statement = null;
 
     public Util() {
     }
@@ -16,19 +13,13 @@ public class Util {
         String USERNAME = "root";
         String PASSWORD = "root";
 
-        connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
         Class.forName("com.mysql.cj.jdbc.Driver");
 
-        return connection;
+        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
     }
 
     public Statement getStatement(Connection connection) throws SQLException {
-        statement = connection.createStatement();
-        return statement;
+        return connection.createStatement();
     }
 
-    public void close() throws SQLException, ClassNotFoundException {
-        statement.close();
-        connection.close();
-    }
 }
